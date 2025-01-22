e2c221786895bbcdfd68143cb65a98bc01044d4e
Tri <tritac.le@gmail.com>
2025-01-22 22:38:06 +0100

    added script and client

---
+++ A3/src/main/java/A3/group/commands/Client.java
@@ -0,0 +1,85 @@
+package A3.group.commands;
+
+import A3.group.commands.CapitalizeTextCommand.CapitalizeWordsTextCommand;
+import A3.group.commands.ReplaceTextCommand.ReplaceFirstTextCommand;
+import A3.group.commands.TextCommandBase.CapitalizedTextCommand;
+import A3.group.commands.TextCommandBase.ReplaceTextCommand;
+import A3.group.commands.TextCommandBase.WrapTextCommand;
+import java.util.*;
+
+/**
+ * Main class
+ */
+public class Client {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    List<TextCommand> commands = new ArrayList<>(); //Deque<TextCommand>
+    
+    System.out.println("Text manipulating API");
+    System.out.println("Write text:");
+    String inputText = scanner.nextLine();
+    
+    while (true) {
+      System.out.println("\nChose commando:");
+      System.out.println("1. ReplaceTextCommand");
+      System.out.println("2. ReplaceFirstTextCommand");
+      System.out.println("3. WrapTextCommand");
+      System.out.println("4. CapitalizeTextCommand");
+      System.out.println("5. CapitalizeWordsTextCommand");
+      System.out.println("6. Script");
+      System.out.println("7. Exit");
+      
+      int choice;
+      try {
+        choice = scanner.nextInt();
+        scanner.nextLine();
+      } catch (InputMismatchException e) {
+        System.out.println("Try again.");
+        scanner.nextLine();
+        continue;
+      }
+      switch (choice) {
+        case 1 -> {
+          System.out.println("Write target text:");
+          String target = scanner.nextLine();
+          System.out.println("Write replacement text:");
+          String replacement = scanner.nextLine();
+          commands.add(new ReplaceTextCommand(target, replacement));
+        }
+        case 2 -> {
+          System.out.println("Write target text:");
+          String target = scanner.nextLine();
+          System.out.println("Write replacement text:");
+          String replacement = scanner.nextLine();
+          commands.add(new ReplaceFirstTextCommand(target, replacement));
+        }
+        case 3 -> {
+          System.out.println("Write opening text:");
+          String opening = scanner.nextLine();
+          System.out.println("Write end text:");
+          String end = scanner.nextLine();
+          commands.add(new WrapTextCommand(opening, end));
+        }
+        case 4 -> commands.add(new CapitalizedTextCommand());
+        case 5 -> {
+          System.out.println("Write word:");
+          String word = scanner.nextLine();
+          commands.add(new CapitalizeWordsTextCommand(word));
+        }
+        case 6 -> {
+          Script script = new Script(commands);
+          String result = script.execute(inputText);
+          System.out.println("Result:");
+          System.out.println(result);
+        }
+        case 7 -> {
+          System.out.println("The end of the script");
+          scanner.close();
+          return;
+        }
+        default -> System.out.println("Try again!!");
+      }
+    }
+  }
+}
--- A3/src/main/java/A3/group/commands/Script.java
+++ A3/src/main/java/A3/group/commands/Script.java
@@ -1,19 +1,21 @@
-package A3.group.commands.TextCommandBase;
+package A3.group.commands;
 import java.util.ArrayList;
 import java.util.List;
-
-import A3.group.commands.TextCommand;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Script {
   private final List<TextCommand> textCommands;
-  
 
-  public Script(){
-    textCommands = new ArrayList<>();
+  public Script(List<TextCommand> textCommands) {
+    this.textCommands = textCommands;
   }
-
+  
+  /**
+   * Executes text commands
+   * @param text
+   * @return
+   */
   public String execute(String text){
     if(text == null || text.isBlank()){
       throw new IllegalArgumentException("Input text cannot be null or empty");
